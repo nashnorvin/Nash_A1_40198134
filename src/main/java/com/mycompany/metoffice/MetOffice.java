ebc56d6b4afbb0c14231a738866974fbc3537074
40198134 <40198134@HY-R104-21.resource.uc>
2026-01-27 13:56:00 +0000

    added the data, years months and temp.

--- src/main/java/com/mycompany/metoffice/MetOffice.java
+++ src/main/java/com/mycompany/metoffice/MetOffice.java
@@ -18,9 +18,52 @@ public class MetOffice {
         System.out.println("Temprature in London, 2023-2024");
         
         months[0]="Jannuary";
-        temprature23[0]="8";
-        temprature24[0]="8";
+        temprature23[0]= 8;
+        temprature24[0]= 8;
         
+        months[1]="February";
+        temprature23[1]= 9;
+        temprature24[1]= 8;
+        
+        months[2]="March";
+        temprature23[2]= 12;
+        temprature24[2]= 12;
+        
+        months[3]="April";
+        temprature23[3]= 16;
+        temprature24[3]= 17;
+        
+        months[4]="May";
+        temprature23[4]= 18;
+        temprature24[4]= 19;
+        
+        months[5]="June";
+        temprature23[5]= 22;
+        temprature24[5]= 20;
+        
+        months[6]="July";
+        temprature23[6]= 24;
+        temprature24[6]= 23;
+        
+        months[7]="August";
+        temprature23[7]= 22;
+        temprature24[7]= 22;
+        
+        months[8]="September";
+        temprature23[8]= 20;
+        temprature24[8]= 19;
+        
+        months[9]="October";
+        temprature23[9]= 16;
+        temprature24[9]= 16;
+        
+        months[10]="November";
+        temprature23[10]= 12;
+        temprature24[10]= 10;
+        
+        months[11]="December";
+        temprature23[11]= 9;
+        temprature24[11]= 8;
         
         
         //linear search
