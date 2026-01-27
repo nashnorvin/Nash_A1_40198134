b9a51637fa554020f3f4a28da5cfa1a645df691e
40198134 <40198134@HY-R104-21.resource.uc>
2026-01-27 13:05:12 +0000

    Starting the assignment

---
+++ src/main/java/com/mycompany/metoffice/MetOffice.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.metoffice;
+
+/**
+ *
+ * @author 40198134
+ */
+import java.util.Scanner;
+public class MetOffice {
+
+    public static void main(String[] args) {
+        String[] months= new String[12];
+        String[] temprature23 = new String
+        String[] temprature24 =       
+        System.out.println("Weather station report");
+        
+        
+        //linear search
+        /*String entername;
+        Scanner I=new Scanner(System.in);
+        System.out.println("Who are you looking for? ");
+        entername = I.next();
+        for(int i=0; i<12; ++){
+            if entername.equals(Classlist[i]){
+            System.out.println("name found" +i);
+            }
+        }
+        */
+        //average
+        /*int total=0;
+        int average=0
+        for (int i=0; i<12; i++){
+            total = total+ agelist[i];
+        }
+        average = total/12;
+        System.out.println(" the average temrature is :" average);
+        */
+        
+    }
+}
