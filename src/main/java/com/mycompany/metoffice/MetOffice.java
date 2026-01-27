7387ac9c590e19701a8800863ec0da1ad68134f4
40198134 <40198134@HY-R104-21.resource.uc>
2026-01-27 13:37:07 +0000

    Starting the assignment

--- src/main/java/com/mycompany/metoffice/MetOffice.java
+++ src/main/java/com/mycompany/metoffice/MetOffice.java
@@ -13,9 +13,14 @@ public class MetOffice {
 
     public static void main(String[] args) {
         String[] months= new String[12];
-        String[] temprature23 = new String
-        String[] temprature24 =       
-        System.out.println("Weather station report");
+        int[] temprature23 = new int[12];
+        int[] temprature24 = new int[12];     
+        System.out.println("Temprature in London, 2023-2024");
+        
+        months[0]="Jannuary";
+        temprature23[0]="8";
+        temprature24[0]="8";
+        
         
         
         //linear search
@@ -24,7 +29,7 @@ public class MetOffice {
         System.out.println("Who are you looking for? ");
         entername = I.next();
         for(int i=0; i<12; ++){
-            if entername.equals(Classlist[i]){
+            if (entername.equals(Classlist[i])){
             System.out.println("name found" +i);
             }
         }
