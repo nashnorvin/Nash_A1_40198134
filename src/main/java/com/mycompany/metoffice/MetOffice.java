ee702892c89023bf9ad8d6ca1094383a8fd53e0b
40198134 <40198134@HY-R104-21.resource.uc>
2026-02-03 13:31:46 +0000

    added the data, years months and temp.

--- src/main/java/com/mycompany/metoffice/MetOffice.java
+++ src/main/java/com/mycompany/metoffice/MetOffice.java
@@ -12,80 +12,48 @@ import java.util.Scanner;
 public class MetOffice {
 
     public static void main(String[] args) {
-        String[] months= new String[12];
-        int[] temprature23 = new int[12];
-        int[] temprature24 = new int[12];     
+        String[] months= {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"};
+        int[] temprature23 = {8, 9, 12, 16, 18, 22, 24, 22, 20, 16, 12, 9};
+        int[] temprature24 = {8, 9, 12, 17, 19, 20, 23, 22, 19, 16, 10, 8};   
+        Scanner s = new Scanner (System.in); 
+        String searchMonth;
+        
         System.out.println("Temprature in London, 2023-2024");
+       
         
-        months[0]="Jannuary";
-        temprature23[0]= 8;
-        temprature24[0]= 8;
-        
-        months[1]="February";
-        temprature23[1]= 9;
-        temprature24[1]= 8;
-        
-        months[2]="March";
-        temprature23[2]= 12;
-        temprature24[2]= 12;
-        
-        months[3]="April";
-        temprature23[3]= 16;
-        temprature24[3]= 17;
-        
-        months[4]="May";
-        temprature23[4]= 18;
-        temprature24[4]= 19;
-        
-        months[5]="June";
-        temprature23[5]= 22;
-        temprature24[5]= 20;
-        
-        months[6]="July";
-        temprature23[6]= 24;
-        temprature24[6]= 23;
-        
-        months[7]="August";
-        temprature23[7]= 22;
-        temprature24[7]= 22;
-        
-        months[8]="September";
-        temprature23[8]= 20;
-        temprature24[8]= 19;
-        
-        months[9]="October";
-        temprature23[9]= 16;
-        temprature24[9]= 16;
-        
-        months[10]="November";
-        temprature23[10]= 12;
-        temprature24[10]= 10;
-        
-        months[11]="December";
-        temprature23[11]= 9;
-        temprature24[11]= 8;
-        
-        
-        //linear search
-        /*String entername;
-        Scanner I=new Scanner(System.in);
-        System.out.println("Who are you looking for? ");
-        entername = I.next();
-        for(int i=0; i<12; ++){
-            if (entername.equals(Classlist[i])){
-            System.out.println("name found" +i);
-            }
-        }
-        */
-        //average
-        /*int total=0;
-        int average=0
         for (int i=0; i<12; i++){
-            total = total+ agelist[i];
+            System.out.println("Month: "+ months[i] + " " + "Temprature in 2023: " + temprature23[i] + " " + "Temprature in 2024: "+ temprature24[i]);
+        }
+            
+         //average temprature for 2023
+        int total=0;
+        int average=0;
+        for (int i=0; i<12; i++){
+            total = total+ temprature23[i];
         }
         average = total/12;
-        System.out.println(" the average temrature is :" average);
-        */
+        System.out.println("The average temrature for 2023 is :" + average);
         
+        //average temprature for 2024
+        int total2=0;
+        int average2=0;
+        for (int i=0; i<12; i++){
+            total = total+ temprature24[i];
+        }
+        average = total/12;
+        System.out.println("The average temrature for 2024 is :" + average);
+        
+        //linear search
+        String entername;
+        System.out.println("Which month do you want to look for? ");
+        searchMonth = s.next();
+        for(int i=0; i<12; i++){
+            if (searchMonth.equals(months[i])){
+            System.out.println("Month found:"+ " " + months[i] + " " + "Temprature in 2023: " + temprature23[i] + " "+ "Temprature in 2024: " + temprature24[i]);
+            }
+        }
+        
+        
+       
     }
 }
